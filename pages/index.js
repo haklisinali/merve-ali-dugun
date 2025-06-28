b23ba5fbd3ef80acd0ed0a0af37b8065d4bb5b35
haklisinali <tarlaci7038@gmail.com>
2025-06-28 10:07:21 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
+      <h1>Merve ve Ali Düğün</h1>
+      <p>Anılarımız</p>
+      <p>Bu proje Next.js ile hazırlanmış bir galeridir.</p>
+    </div>
+  );
+}
